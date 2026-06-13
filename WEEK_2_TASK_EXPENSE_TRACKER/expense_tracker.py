338afd5889e20139849ed5c9c9a952b3a93ef7e1
Raunit singh <raunitsingh934@gmail.com>
2026-06-13 18:02:11 +0530

    Add files via upload

---
+++ WEEK_2_TASK_EXPENSE_TRACKER/expense_tracker.py
@@ -0,0 +1,12 @@
+total=0
+
+expense=int(input("Enter first expense: "))
+total += expense
+
+expense=int(input("Enter second expense: "))
+total += expense
+
+expense=int(input("Enter third expense: "))
+total += expense
+
+print("Total spent =", total)
